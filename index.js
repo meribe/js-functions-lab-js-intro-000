5859ea98aea240e427e7a0aed2569eb06c7f3e09
witty-abstraction-1855 <merve.kastan4@gmail.com>
2020-10-30 17:42:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // write your code below!
+function happyHolidays() {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(){
+  var name = "Merve"
+  return `Happy holidays,  $(name)!`
+}
+function happyCustomHolidayTo() {
+  var holiday= "halloween"
+  var name= "merve"
+  return `Happy $(holiday),  $(name)!`
+}
+function holidayCountdown() {
+  var days="16"
+  var holiday= "halloween"
+  return `It's ${days} days until ${holiday}!`
+}
